0ce6e8983fdfaa23507c903fb6c5c83e75f196cb
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-06-13 18:46:20 +0200

    Add files via upload

---
+++ DrugiZadatakk/DrugiZadatakk/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugiZadatakk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaD MainObjekt = new KlasaD();
+            bool Uvjet = MainObjekt.Suprotno(true);
+            int Broj = MainObjekt.Suprotno(50);
+            Console.WriteLine("Suprotno od True je =  " + Uvjet);
+            Console.WriteLine("Suprotno od 50 je =  " + Broj);
+
+            Console.ReadKey();
+        }
+    }
+}
